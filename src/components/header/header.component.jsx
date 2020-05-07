264fa3c4013450b29527955bef05487b692b1922
swati singh <swati175singh@gmail.com>
2020-05-07 16:11:20 +0530

    added acrt icon and carticon dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from '../../assests/crown.svg';
+import CartIcon from '../cart-icon//cart-icon.component';
+import CartDropdowm from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className= 'logo-container' to="/">
             <Logo className= 'logo' />
@@ -21,12 +23,17 @@ const Header = ({ currentUser }) => (
         {
             currentUser ?
             <div className='option' onClick={() => auth.signOut()}> SIGN OUT</div> :
-            <Link className='option'>SIGN IN</Link>
+            <Link className='option' to="/signin">SIGN IN</Link>
         }
+        <CartIcon />
     </div>
+        { hidden ? null : <CartDropdowm /> }
+        
     </div>
 );
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
